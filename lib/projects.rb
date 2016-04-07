4e3a0c8073ca49e8f906b19bbc36b17df78d8786
McN33ly <rmcneely23@gmail.com>
2016-04-07 01:30:12 -0400

    Done.

---
+++ lib/projects.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Project
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+      @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,14 +4,22 @@ require 'open-uri'
 
 class Scraper
 
+  attr_accessor :projects_list
+
   def self.fresh_scrape
-    scraped_url = "http://forum.xda-developers.com/lg-g3/development#romList"
+    scraped_url = "http://forum.xda-developers.com/lg-g3/development"
     scraped = Nokogiri::HTML(open(scraped_url))
 
-    rom_list = []
-    scraped.css("div #tab_romList")
-
-binding.pry
+    @projects_list = []
+    #scraped.css("div #tab_romList").each do |div|
+    scraped.css(".thread-title-cell").each do |entry|
+      @projects_list << entry.css(".devdb-tag + .threadTitle").text
+    end
+    @projects_list.delete_if {|thread| thread == ""}
+#binding.pry
   end
 
+  def self.projects_list
+    @projects_list
+  end
 end
