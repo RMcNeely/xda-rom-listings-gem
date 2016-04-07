c27919a38de88dff7904568f7e1202923ff8d2c6
McN33ly <rmcneely23@gmail.com>
2016-04-07 10:32:00 -0400

    Done.

--- lib/projects.rb
+++ lib/projects.rb
@@ -3,12 +3,24 @@ require 'pry'
 class Project
   attr_accessor :name
   @@all = []
+  @roms = []
+  @kernals = []
+  @other_projects = []
   def initialize(name)
     @name = name
-      @@all << self
+    if @name.includes?("ROM")
+      @roms << self
+    elsif @name.includes?("KERNAL")
+      @kernals << self
+    else
+      @other_projects << self
+    end  
+    @@all << self
   end
 
   def self.all
     @@all
   end
+
+
 end
